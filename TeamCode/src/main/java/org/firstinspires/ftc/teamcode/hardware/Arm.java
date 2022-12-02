7ff375df548e80a49a1504a141380ffe12fa2ae3
Zawad Dewan <zawaddewan@gmail.com>
2022-12-02 12:45:00 -0500

    array index out of deez nutz

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Arm.java
@@ -10,32 +10,37 @@ public class Arm extends Mechanism{
     private DcMotor motors[] = new DcMotor[2];
 
     private double tpr = 751.8;
-    static public int kP = 1;
-    static public int kD = 0;
-    static public int kI = 0;
+    static public double kP = 1;
+    static public double kD = 0;
+    static public double kI = 0;
+    static public double kCos = 0;
     public static PIDCoefficients coeffs = new PIDCoefficients(kP, kD, kI);
-    public static PIDFController controller = new PIDFController(coeffs);
+    public static PIDFController controller = new PIDFController(coeffs, 0, 0, 0, (angle, angularVel) -> kCos*Math.cos(angle));
 
     @Override
     public void init(HardwareMap hwMap) {
-        motors[1] = hwMap.get(DcMotor.class, "arm1");
-        motors[2] = hwMap.get(DcMotor.class, "arm2");
+        motors[0] = hwMap.get(DcMotor.class, "arm1");
+        motors[1] = hwMap.get(DcMotor.class, "arm2");
+        motors[0].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motors[1].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motors[2].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motors[0].setDirection(DcMotorSimple.Direction.FORWARD);
         motors[1].setDirection(DcMotorSimple.Direction.FORWARD);
-        motors[2].setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
-    public void update(double angle, int motor) {
+    public void setPos(double angle, int motor) {
         double position = angleToPos(angle);
         controller.setTargetPosition(position);
         motors[motor].setPower(controller.update(motors[motor].getCurrentPosition()));
 
     }
 
+    public void update() {
+
+    }
+
     public double posToAngle(int motor) {
         double angle = 0;
-        return motors[motor].getCurrentPosition() / tpr * 360;
+        return motors[motor-1].getCurrentPosition() / tpr * 360;
     }
 
     public double angleToPos(double angle) {
@@ -43,11 +48,11 @@ public class Arm extends Mechanism{
     }
 
     public void power(double speed, int motor) {
-        motors[motor].setPower(speed);
+        motors[motor-1].setPower(speed);
     }
 
     public double getAngle(int motor) {
-        return angleToPos(motors[motor].getCurrentPosition());
+        return angleToPos(motors[motor-1].getCurrentPosition());
     }
 
 }
