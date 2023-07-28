ac470664dcf81331b960dc0d82337325851eb4b1
pyshrekek <rukereero@gmail.com>
2023-07-27 20:46:42 -0400

    update robot controller to 8.2.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Turret.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+
 @Config
 public class Turret extends Mechanism{
     public DcMotorEx turrs[] = new DcMotorEx[2];
